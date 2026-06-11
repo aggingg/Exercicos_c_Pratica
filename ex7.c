6b798302671d4a2046eecab504e4e85029e8746c
gwenzinha <gwenbonequinhalindinha@gmail.com>
2026-06-11 12:31:00 -0300

    Update ex7.c

---
+++ ex7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float peso, altura, imc;
+
+    printf("Digite seu peso (kg): ");
+    scanf("%f", &peso);
+
+    printf("Digite sua altura (m): ");
+    scanf("%f", &altura);
+
+    imc = peso / (altura * altura);
+
+    printf("Seu IMC e: %.2f\n", imc);
+
+    return 0;
+}
